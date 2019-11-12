a0e6eb032c5107cdbeb1ada2b0dc2b4a748dc0a6
Angela Vawser <amvawser@gmail.com>
2019-11-12 15:07:22 -0800

    whoops, first commit...action, reducer and selector done

---
+++ src/selectors/moodSelector.js
@@ -0,0 +1,33 @@
+export const getCoffees = state => state.coffees;
+export const getSnacks = state => state.snacks;
+export const getNaps = state => state.naps;
+export const getStudies = state => state.studies;
+export const getReset = state => state;
+
+export const getActions = state => [
+  { name: 'DRINK_COFFEE', text: 'Drink Coffee', count: getCoffees(state) },
+  { name: 'EAT_SNACK', text: 'Snack', count: getSnacks(state) },
+  { name: 'TAKE_NAP', text: 'Nap', count: getNaps(state) },
+  { name: 'STUDY', text: 'Study', count: getStudies(state) },
+  { name: 'RESET', text: 'Reset' }
+];
+
+export const isTired = state => getCoffees(state) < 1 && getNaps(state) < 1;
+export const isHyper = state => getCoffees(state) > 3;
+export const isEducated = state => getStudies(state) > 2;
+export const isHungry = state => getSnacks(state) < 1;
+export const isSatisfied = state => getSnacks(state) > 3;
+export const isAsleep = state => getNaps(state) > 3;
+
+export const getFace = state => {
+  if(isTired(state) && isHungry(state)) return '🤬';
+  if(isHyper(state) && isHungry(state)) return '🤮';
+  if(isTired(state)) return '😴';
+  if(isAsleep(state)) return '😪';
+  if(isHyper(state)) return '🙀';
+  if(isEducated(state)) return '🤯';
+  if(isHungry(state)) return '😡';
+  if(isSatisfied(state)) return '😋';
+
+  return '😀';
+};
